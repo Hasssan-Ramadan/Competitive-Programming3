2368722e4578dd14ee56d3a21d38b5e0f7563407
Hassan Ramadan <HassanRamadanEbrahim@gmail.com>
2020-11-24 16:29:41 +0200

    Add medium problems' solutions category

---
+++ 1-Introduction/1-Getting-Started/3-Medium/UVa-11507.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    freopen("input.in","r",stdin);
+    int n;
+    char cmd[100000][3];
+    while (scanf("%d", &n) == 1 && n)
+    {
+        for (int i = 0; i < n-1; i++)
+            scanf("%s", cmd[i]);
+        int dir = 0;
+        //+x(0), -x(1), +y(2), -y(3), +z(4), -z(5)
+        for (int i = 0; i < n; i++)
+        {
+            if (!strcmp(cmd[i], "No"))
+                continue;
+            if (!strcmp(cmd[i], "+z"))
+            {
+                if (dir == 0)
+                    dir = 4; //+x->+z
+                else if (dir == 1)
+                    dir = 5; //-x->-z
+                else if (dir == 4)
+                    dir = 1; //+z->-x
+                else if (dir == 5)
+                    dir = 0; //-z->+x
+            }
+            if (!strcmp(cmd[i], "-z"))
+            {
+                if (dir == 0)
+                    dir = 5; //+x->-z
+                else if (dir == 1)
+                    dir = 4; //-x->+z
+                else if (dir == 4)
+                    dir = 0; //+z->+x
+                else if (dir == 5)
+                    dir = 1; //-z->-x
+            }
+            if (!strcmp(cmd[i], "+y"))
+            {
+                if (dir == 0)
+                    dir = 2; //+x->+y
+                else if (dir == 1)
+                    dir = 3; //-x->-y
+                else if (dir == 2)
+                    dir = 1; //+y->-x
+                else if (dir == 3)
+                    dir = 0; //-y->+x
+            }
+            if (!strcmp(cmd[i], "-y"))
+            {
+                if (dir == 0)
+                    dir = 3; //+x->-y
+                else if (dir == 1)
+                    dir = 2; //-x->+y
+                else if (dir == 2)
+                    dir = 0; //+y->+x
+                else if (dir == 3)
+                    dir = 1; //-y->-x
+            }
+        }
+        if (dir == 0)
+            puts("+x");
+        if (dir == 1)
+            puts("-x");
+        if (dir == 2)
+            puts("+y");
+        if (dir == 3)
+            puts("-y");
+        if (dir == 4)
+            puts("+z");
+        if (dir == 5)
+            puts("-z");
+    }
+    return 0;
+}
