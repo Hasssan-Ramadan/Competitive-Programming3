3c48a10ee4fd7918d7d989e6da306e9632c2849e
Hassan Ramadan <HassanRamadanEbrahim@gmail.com>
2020-11-24 19:14:55 +0200

    fix uva-11507 solution

--- 1-Introduction/1-Getting-Started/3-Medium/UVa-11507.cpp
+++ 1-Introduction/1-Getting-Started/3-Medium/UVa-11507.cpp
@@ -1,78 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+long L;
+string dir, wire;
+map<string, map<string, string>> bends;
+
 int main()
 {
-    freopen("input.in","r",stdin);
-    int n;
-    char cmd[100000][3];
-    while (scanf("%d", &n) == 1 && n)
+    bends["+x"]["+y"] = "+y";
+    bends["+x"]["-y"] = "-y";
+    bends["+x"]["+z"] = "+z";
+    bends["+x"]["-z"] = "-z";
+    bends["-x"]["+y"] = "-y";
+    bends["-x"]["-y"] = "+y";
+    bends["-x"]["+z"] = "-z";
+    bends["-x"]["-z"] = "+z";
+    bends["+y"]["+y"] = "-x";
+    bends["+y"]["-y"] = "+x";
+    bends["+y"]["+z"] = "+y";
+    bends["+y"]["-z"] = "+y";
+    bends["-y"]["+y"] = "+x";
+    bends["-y"]["-y"] = "-x";
+    bends["-y"]["+z"] = "-y";
+    bends["-y"]["-z"] = "-y";
+    bends["+z"]["+y"] = "+z";
+    bends["+z"]["-y"] = "+z";
+    bends["+z"]["+z"] = "-x";
+    bends["+z"]["-z"] = "+x";
+    bends["-z"]["+y"] = "-z";
+    bends["-z"]["-y"] = "-z";
+    bends["-z"]["+z"] = "+x";
+    bends["-z"]["-z"] = "-x";
+
+    for (;;)
     {
-        for (int i = 0; i < n-1; i++)
-            scanf("%s", cmd[i]);
-        int dir = 0;
-        //+x(0), -x(1), +y(2), -y(3), +z(4), -z(5)
-        for (int i = 0; i < n; i++)
+        scanf("%d", &L);
+        if (L == 0)
+            break;
+        L--;
+        wire = "+x";
+        for (long i = 0; i < L; i++)
         {
-            if (!strcmp(cmd[i], "No"))
-                continue;
-            if (!strcmp(cmd[i], "+z"))
-            {
-                if (dir == 0)
-                    dir = 4; //+x->+z
-                else if (dir == 1)
-                    dir = 5; //-x->-z
-                else if (dir == 4)
-                    dir = 1; //+z->-x
-                else if (dir == 5)
-                    dir = 0; //-z->+x
-            }
-            if (!strcmp(cmd[i], "-z"))
-            {
-                if (dir == 0)
-                    dir = 5; //+x->-z
-                else if (dir == 1)
-                    dir = 4; //-x->+z
-                else if (dir == 4)
-                    dir = 0; //+z->+x
-                else if (dir == 5)
-                    dir = 1; //-z->-x
-            }
-            if (!strcmp(cmd[i], "+y"))
-            {
-                if (dir == 0)
-                    dir = 2; //+x->+y
-                else if (dir == 1)
-                    dir = 3; //-x->-y
-                else if (dir == 2)
-                    dir = 1; //+y->-x
-                else if (dir == 3)
-                    dir = 0; //-y->+x
-            }
-            if (!strcmp(cmd[i], "-y"))
-            {
-                if (dir == 0)
-                    dir = 3; //+x->-y
-                else if (dir == 1)
-                    dir = 2; //-x->+y
-                else if (dir == 2)
-                    dir = 0; //+y->+x
-                else if (dir == 3)
-                    dir = 1; //-y->-x
-            }
+            cin >> dir;
+            if (dir != "No")
+                wire = bends[wire][dir];
         }
-        if (dir == 0)
-            puts("+x");
-        if (dir == 1)
-            puts("-x");
-        if (dir == 2)
-            puts("+y");
-        if (dir == 3)
-            puts("-y");
-        if (dir == 4)
-            puts("+z");
-        if (dir == 5)
-            puts("-z");
+        cout << wire << endl;
     }
-    return 0;
 }
