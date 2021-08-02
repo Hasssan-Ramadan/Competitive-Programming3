8048a1f2564b1e82fc2e538e40318e01afa179b7
Hassan Ramadan <HassanRamadanEbrahim@gmail.com>
2021-08-02 05:45:21 +0200

    Add complete search - recursive problems

---
+++ 3-Problem-Solving-Paradigms/1-Complete-Search/UVa-00750.cpp
@@ -0,0 +1,178 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+using vi = vector<int>;
+#define pb push_back
+#define rsz resize
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+using pi = pair<int, int>;
+#define f first
+#define s second
+#define mp make_pair
+
+#define LSOne(S) (S & (-S))
+
+void setIO(string name = "")
+{
+ ios_base::sync_with_stdio(0);
+ cin.tie(0);
+
+ if (sz(name))
+ {
+  // freopen((name + ".in").c_str(), "r", stdin);
+  freopen((name + ".out").c_str(), "w", stdout);
+ }
+}
+
+class UnionFind
+{
+private:
+ vi p, rank;
+
+public:
+ UnionFind(int N)
+ {
+  rank.assign(N, 0);
+  p.assign(N, 0);
+  for (int i = 0; i < N; i++)
+   p[i] = i;
+ }
+ int findSet(int i) { return (p[i] == i) ? i : (p[i] = findSet(p[i])); }
+ bool isSameSet(int i, int j) { return findSet(i) == findSet(j); }
+ void unionSet(int i, int j)
+ {
+  if (!isSameSet(i, j))
+  {
+   int x = findSet(i), y = findSet(j);
+   if (rank[x] > rank[y])
+    p[y] = x;
+   else
+   {
+    p[x] = y;
+    if (rank[x] == rank[y])
+     rank[y]++;
+   }
+  }
+ }
+};
+
+class SegmentTree
+{
+private:
+ vi st, A;
+ int n;
+ int left(int p) { return p << 1; }
+ int right(int p) { return (p << 1) + 1; }
+ void build(int p, int L, int R)
+ {
+  if (L == R)
+   st[p] = L;
+  else
+  {
+   build(left(p), L, (L + R) / 2);
+   build(right(p), (L + R) / 2 + 1, R);
+   int p1 = st[left(p)], p2 = st[right(p)];
+   st[p] = (A[p1] <= A[p2]) ? p1 : p2;
+  }
+ }
+ int rmq(int p, int L, int R, int i, int j)
+ {
+  if (i > R || j < L)
+   return -1;
+  if (L >= i && R <= j)
+   return st[p];
+  int p1 = rmq(left(p), L, (L + R) / 2, i, j);
+  int p2 = rmq(right(p), (L + R) / 2 + 1, R, i, j);
+  if (p1 == -1)
+   return p2;
+  if (p2 == -1)
+   return p1;
+  return (A[p1] <= A[p2]) ? p1 : p2;
+ }
+
+public:
+ SegmentTree(const vi &_A)
+ {
+  A = _A;
+  n = (int)A.size();
+  st.assign(4 * n, 0);
+  build(1, 0, n - 1);
+ }
+ int rmq(int i, int j) { return rmq(1, 0, n - 1, i, j); }
+};
+
+class FenwickTree
+{
+private:
+ vi ft;
+
+public:
+ FenwickTree() {}
+ FenwickTree(int n) { ft.assign(n + 1, 0); }
+ int rsq(int b)
+ {
+  int sum = 0;
+  for (; b; b -= LSOne(b))
+   sum += ft[b];
+  return sum;
+ }
+ int rsq(int a, int b)
+ {
+  return rsq(b) - (a == 1 ? 0 : rsq(a - 1));
+ }
+ void adjust(int k, int v)
+ {
+  for (; k < (int)ft.size(); k += LSOne(k))
+   ft[k] += v;
+ }
+};
+
+int row[8], TC, a, b, lineCounter;
+
+bool place(int r, int c)
+{
+ for (int prev = 0; prev < c; prev++)
+  if (row[prev] == r || (abs(row[prev] - r) == abs(prev - c)))
+   return false;
+ return true;
+}
+
+void backtrack(int c)
+{
+ if (c == 8 && row[b] == a)
+ {
+  printf("%2d     ", ++lineCounter);
+  for (int j = 0; j < 8; j++)
+   printf(" %d", row[j] + 1);
+  printf("\n");
+ }
+ for (int r = 0; r < 8; r++)
+  if (place(r, c))
+  {
+   row[c] = r;
+   backtrack(c + 1);
+  }
+}
+
+int main()
+{
+ setIO();
+ scanf("%d", &TC);
+ while (TC--)
+ {
+  scanf("%d %d", &a, &b);
+  a--;
+  b--;
+  memset(row, 0, sizeof row);
+  lineCounter = 0;
+  printf("SOLN       COLUMN\n");
+  printf(" #      1 2 3 4 5 6 7 8\n\n");
+  backtrack(0);
+  if (TC)
+   printf("\n");
+ }
+}
